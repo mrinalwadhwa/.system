7e4b7b2e261ea0ff2c7023221cba05ac59d41ac7
Mrinal Wadhwa <mwadhwa@fybr-tech.com>
2016-08-08 02:02:07 +0530

    Merge brew.sh and init.sh, also add virtualization

--- init.sh
+++ init.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
 
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install GNU core utilities (those that come with macOS are outdated).
+brew install coreutils
+ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+brew tap caskroom/cask
+
+
 # --------- Shell ---------
+
+# Install Bash
+brew install bash
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
 
 touch ".hushlogin"
 
@@ -10,10 +32,35 @@ ln -s "${HOME}/.system/dotfiles/.bashrc"
 rm -f ".bash_profile"
 ln -s "${HOME}/.system/dotfiles/.bash_profile"
 
-# --------- Editor Config ---------
+
+# --------- Editors ---------
+
+brew cask install atom
+brew cask install textmate
 
 rm -f ".editorconfig"
 ln -s "${HOME}/.system/dotfiles/.editorconfig"
 
 rm -f ".tm_properties"
 ln -s "${HOME}/.system/dotfiles/.tm_properties"
+
+
+# --------- Networking ---------
+
+brew cask install nmap
+
+
+# --------- Virtualization ---------
+
+brew cask install virtualbox
+brew cask install vagrant
+
+source "${HOME}/.system/profile.d/vagrant.sh"
+mkdir -p "$VAGRANT_HOME"
+cp "$HOME/.system/dotfiles/.vagrant.d/Vagrantfile" "$VAGRANT_HOME/Vagrantfile"
+
+
+# ----------------------------------
+
+# Remove outdated versions from the cellar.
+brew cleanup
