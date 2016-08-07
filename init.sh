c8759e0050a5eb99a546953aac5ce82f07b505e5
Mrinal Wadhwa <mwadhwa@fybr-tech.com>
2016-08-08 01:16:50 +0530

    Add Editor Config

--- init.sh
+++ init.sh
@@ -9,3 +9,11 @@ ln -s "${HOME}/.system/dotfiles/.bashrc"
 
 rm -f ".bash_profile"
 ln -s "${HOME}/.system/dotfiles/.bash_profile"
+
+# --------- Editor Config ---------
+
+rm -f ".editorconfig"
+ln -s "${HOME}/.system/dotfiles/.editorconfig"
+
+rm -f ".tm_properties"
+ln -s "${HOME}/.system/dotfiles/.tm_properties"
